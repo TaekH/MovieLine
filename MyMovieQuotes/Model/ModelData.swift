bbeed0ca6867c2bbce4bcc9172508468e09a6f12
TaekhwanHan <hth3396@gmail.com>
2022-04-28 18:00:20 +0900

    [ADD] 더미데이터 추가 (임시)

---
+++ MyMovieQuotes/Model/ModelData.swift
@@ -0,0 +1,34 @@
+//
+//  ModelData.swift
+//  MyMovieQuotes
+//
+//  Created by 한택환 on 2022/04/28.
+//
+//MARK: .json 파싱
+
+import Foundation
+import Combine
+
+var movieInfo: [MovieInfo] = load("Movies.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
---
+++ MyMovieQuotes/Model/MovieInfo.swift
@@ -0,0 +1,41 @@
+//
+//  MovieInfo.swift
+//  MyMovieQuotes
+//
+//  Created by 한택환 on 2022/04/28.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieInfo: Hashable, Codable, Identifiable {
+    var title: String
+    var cast: String
+    var director: String
+    var quote: String
+    
+    private var posterName: String //이미지 이름을 읽어올 imageName 속성
+    var poster: Image {//이미지 로드
+        Image(posterName)
+    }
+    
+    var trailer: String
+    var starrating: Float
+    var release: Int
+    var id: Int
+}
+
+
+/*
+ {
+     "title": "begin again",
+     "cast": "마크 러팔로, 키이라 나이틀리, 헤일리 스테인펠트"
+     "director": "존 카니"
+     "quote": "이래서 음악이 좋아\n모든 평범함도 음악을 듣는 순간\n빛나는 진주처럼 변하니까."
+     "poster": "beginagainposter"
+     "trailer":" "
+     "starrating": 5
+     "release": 2014
+     "id": 1
+ },
+ */
