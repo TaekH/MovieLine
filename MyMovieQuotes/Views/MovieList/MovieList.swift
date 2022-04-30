861a293fd2a4254ed93c541e6276aec6988bb51d
TaekhwanHan <hth3396@gmail.com>
2022-04-30 23:42:20 +0900

    [CHORE] 네비게이션타이틀 추가

--- MyMovieQuotes/Views/MovieList/MovieList.swift
+++ MyMovieQuotes/Views/MovieList/MovieList.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct MovieList: View {
     var body: some View {
-        Color.gray
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
--- MyMovieQuotes/Views/MovieQuote/MovieQuote.swift
+++ MyMovieQuotes/Views/MovieQuote/MovieQuote.swift
@@ -10,26 +10,30 @@ import SwiftUI
 struct MovieQuote: View {
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-        MovieQuotePoste(movieinfo: movieInfo[0])
-            
-            Text("다른 콘텐츠")
-                .font(.system(size: 15))
-                .fontWeight(.bold)
-                .padding(10)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 0) {
-                    ForEach(0..<10) { index in
-                        MoviePoster(moviePoster: movieInfo[index])
-                            .padding(.trailing, 10)
+        NavigationView {
+            VStack(alignment: .leading, spacing: 0) {
+                MovieQuotePoste(movieinfo: movieInfo[0])
+                
+                Text("다른 콘텐츠")
+                    .font(.system(size: 15))
+                    .fontWeight(.bold)
+                    .padding(10)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 0) {
+                        ForEach(0..<10) { index in
+                            MoviePoster(moviePoster: movieInfo[index])
+                                .padding(.trailing, 10)
+                        }
                     }
+                    .padding(.leading, 10)
                 }
-                .padding(.leading, 10)
+                Spacer()
             }
-            Spacer()
+            .edgesIgnoringSafeArea(.all)
+            .navigationTitle("영화")
+            .navigationBarTitleDisplayMode(.large)
         }
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
