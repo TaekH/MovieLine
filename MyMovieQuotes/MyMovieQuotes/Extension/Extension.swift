36b1c13ffca1d5a973ffa1f4f766028c2732f889
TaekhwanHan <hth3396@gmail.com>
2022-05-02 09:25:56 +0900

    [ADD] 크기 Extension 추가

---
+++ MyMovieQuotes/MyMovieQuotes/Extension/Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Extension.swift
+//  MyMovieQuotes
+//
+//  Created by 한택환 on 2022/05/01.
+//
+
+//import Foundation
+//import SwiftUI
+//
+//extension PosterSize{
+//    static let posterWidth = (UIScreen.main.bounds.width - 60) / 2
+//}
