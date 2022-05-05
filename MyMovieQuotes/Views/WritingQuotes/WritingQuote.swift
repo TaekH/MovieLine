e85666322004c2c30ae032bae4fecf5880a410ec
TaekhwanHan <hth3396@gmail.com>
2022-05-05 14:35:32 +0900

    [ADD] WritingQuote 명대사 작성 뷰 추가

---
+++ MyMovieQuotes/Views/WritingQuotes/WritingQuote.swift
@@ -0,0 +1,20 @@
+//
+//  WritingQuote.swift
+//  MyMovieQuotes
+//
+//  Created by 한택환 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct WritingQuote: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WritingQuote_Previews: PreviewProvider {
+    static var previews: some View {
+        WritingQuote()
+    }
+}
